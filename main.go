cbe3cd86b80cc4c6a5f7550ea18cda369b1ab09f
Jason Kao <jasonkao85@gmail.com>
2018-03-01 16:44:19 -0500

    defer to edges & display

--- main.go
+++ main.go
@@ -1,141 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os"
-
 	"./display"
 	"./draw"
-	"./matrix"
 )
 
 func main() {
-	// defer WriteScreenToPPM(screen)
-	// defer draw.DrawLines(matrix, screen)
-
-	// screen = display.NewScreen()
-	// edges, transform := matrix.NewMatrix(), matrix.NewMatrix()
-	m := matrix.NewMatrix()
-	fmt.Println("\nBlank matrix =====")
-	matrix.PrintMatrix(m)
-
-	fmt.Println("Identity matrix =====")
-	matrix.MakeIdentity(m)
-	matrix.PrintMatrix(m)
-
-	m1 := [][]float64{
-		{1, 2, 3},
-		{4, 5, 6},
-	}
-	m2 := [][]float64{
-		{1, 2, 3, 4},
-		{2, 6, 7, 8},
-		{3, 10, 11, 12},
-	}
-	fmt.Println("Matrices to multiply:")
-	matrix.PrintMatrix(m1)
-	matrix.PrintMatrix(m2)
-
-	fmt.Println("Product of multiplication:")
-	matrix.MultiplyMatrices(&m1, &m2)
-	matrix.PrintMatrix(m2)
-
-	drawGopher()
-}
-
-func drawGopher() {
 	screen := display.NewScreen()
-	matrix := [][]float64{
-		{145},
-		{79},
-		{0},
-		{1},
+	edges := [][]float64{
+		{0.0},
+		{0.0},
+		{0.0},
+		{1.0},
 	}
 
-	defer WriteScreenToPPM(screen)
-	defer draw.DrawLines(matrix, screen)
+	defer display.DisplayScreen(screen)
+	defer draw.DrawLines(edges, screen)
 
-	draw.AddEdge(matrix, 142, 82, 0, 162, 70, 0)
-
-	// Yes, I had to build a HTML5 Canvas with an image of the Go Gopher drawn onto it
-	// and use a bit of JS to determine these points.
-	draw.AddPoint(matrix, 189, 52, 0)
-	draw.AddPoint(matrix, 206, 39, 0)
-	draw.AddPoint(matrix, 230, 33, 0)
-	draw.AddPoint(matrix, 254, 33, 0)
-	draw.AddPoint(matrix, 272, 36, 0)
-	draw.AddPoint(matrix, 289, 45, 0)
-	draw.AddPoint(matrix, 298, 40, 0)
-	draw.AddPoint(matrix, 310, 35, 0)
-	draw.AddPoint(matrix, 334, 46, 0)
-	draw.AddPoint(matrix, 338, 61, 0)
-	draw.AddPoint(matrix, 332, 78, 0)
-	draw.AddPoint(matrix, 339, 90, 0)
-	draw.AddPoint(matrix, 353, 119, 0)
-	draw.AddPoint(matrix, 365, 150, 0)
-	draw.AddPoint(matrix, 377, 167, 0)
-	draw.AddPoint(matrix, 383, 187, 0)
-	draw.AddPoint(matrix, 400, 184, 0)
-	draw.AddPoint(matrix, 412, 190, 0)
-	draw.AddPoint(matrix, 406, 205, 0)
-	draw.AddPoint(matrix, 390, 202, 0)
-	draw.AddPoint(matrix, 398, 260, 0)
-	draw.AddPoint(matrix, 399, 294, 0)
-	draw.AddPoint(matrix, 393, 329, 0)
-	draw.AddPoint(matrix, 388, 362, 0)
-	draw.AddPoint(matrix, 400, 366, 0)
-	draw.AddPoint(matrix, 409, 379, 0)
-	draw.AddPoint(matrix, 398, 392, 0)
-	draw.AddPoint(matrix, 391, 382, 0)
-	draw.AddPoint(matrix, 378, 380, 0)
-	draw.AddPoint(matrix, 357, 399, 0)
-	draw.AddPoint(matrix, 330, 412, 0)
-	draw.AddPoint(matrix, 298, 427, 0)
-	draw.AddPoint(matrix, 263, 442, 0)
-	draw.AddPoint(matrix, 241, 443, 0)
-	draw.AddPoint(matrix, 226, 465, 0)
-	draw.AddPoint(matrix, 207, 464, 0)
-	draw.AddPoint(matrix, 214, 438, 0)
-	draw.AddPoint(matrix, 194, 424, 0)
-	draw.AddPoint(matrix, 185, 409, 0)
-	draw.AddPoint(matrix, 178, 414, 0)
-	draw.AddPoint(matrix, 172, 405, 0)
-	draw.AddPoint(matrix, 178, 400, 0)
-	draw.AddPoint(matrix, 173, 384, 0)
-	draw.AddPoint(matrix, 175, 336, 0)
-	draw.AddPoint(matrix, 168, 295, 0)
-	draw.AddPoint(matrix, 160, 266, 0)
-	draw.AddPoint(matrix, 143, 245, 0)
-	draw.AddPoint(matrix, 124, 211, 0)
-	draw.AddPoint(matrix, 117, 193, 0)
-	draw.AddPoint(matrix, 113, 180, 0)
-	draw.AddPoint(matrix, 88, 172, 0)
-	draw.AddPoint(matrix, 83, 156, 0)
-	draw.AddPoint(matrix, 86, 139, 0)
-	draw.AddPoint(matrix, 102, 129, 0)
-	draw.AddPoint(matrix, 112, 128, 0)
-	draw.AddPoint(matrix, 120, 105, 0)
-}
-
-func WriteScreenToPPM(screen [][][]int) {
-	file, err := os.OpenFile("foo.ppm", os.O_CREATE | os.O_WRONLY, 0644)
-
-	if (err != nil) {
-		panic(err)
-	}
-
-	defer file.Close()
-
-	var buffer bytes.Buffer
-	buffer.WriteString(fmt.Sprintf("P3 %d %d 255\n", display.XRES, display.YRES))
-	for i := 0; i < display.YRES; i++ {
-		for j := 0; j < display.
-			XRES; j++ {
-			rgb := screen[i][j]
-			buffer.WriteString(fmt.Sprintf("%d %d %d ", uint8(rgb[0]), uint8(rgb[1]), uint8(rgb[2])))
-		}
-	}
-
-	file.WriteString(buffer.String())
+	draw.AddPoint(edges, 100, 0, 0)
+	draw.AddPoint(edges, 100, 100, 0)
+	draw.AddPoint(edges, 0, 100, 0)
 }
