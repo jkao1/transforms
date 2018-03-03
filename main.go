241afc451cf216de19d83b121b6458f2cdc43fea
Jason Kao <jasonkao85@gmail.com>
2018-03-02 19:07:15 -0500

    gitignore

--- main.go
+++ main.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"./display"
-	"./matrix"
 	"./parser"
 )
 
 func main() {
 	screen := display.NewScreen()
-	transform := matrix.NewMatrix()
-	edges := [][]float64{
-		{0.0},
-		{0.0},
-		{0.0},
-		{1.0},
-	}
+	transform := make([][]float64, 0)
+	edges := make([][]float64, 4)
 
-	// defer display.DisplayScreen(screen)
-	// defer draw.DrawLines(edges, screen)
-
-	parser.ParseFile("test.in", transform, edges, screen)
+	parser.ParseFile("script", transform, edges, screen)
 
 }
